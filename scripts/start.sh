3ad60d25645275187fed877785f37b38b4c16f93
peter dibie <ekene.dibie@gmail.com>
2026-01-30 03:49:40 +0100

    manaka

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source /home/ec2-user/.bash_profile
 cd /var/www/node-app
 # cd /home/ec2-user
 pm2 start app.js --name node-app
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# source /home/ec2-user/.bash_profile
 pm2 stop node-app || true
 pm2 delete node-app || true
