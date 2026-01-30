568c86d1e64c37ad783cae352805cec728404164
peter dibie <ekene.dibie@gmail.com>
2026-01-30 02:16:56 +0100

    eeeee

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /var/www/node-app
 # cd /var/www/node-app #/home/ec2-user
-sudo chmod -R 777 .
+sudo chmod -R 777 /var/www/node-app
 echo "Current User-->: $(whoami)"
 echo "Current Directory-->: $(pwd)"
 npm install
