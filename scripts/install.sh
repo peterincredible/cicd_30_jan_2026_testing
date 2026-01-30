817c09d4654ffa7ebe4280afeab9ea4fc4a656d5
peter dibie <ekene.dibie@gmail.com>
2026-01-30 03:46:35 +0100

    ttt

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
 cd /var/www/node-app
+# sudo chown -R ec2-user:ec2-user /var/www/node-app
 # cd /var/www/node-app #/home/ec2-user
+sudo chown -R ec2-user /var/www/node-app
 echo "Current User-->: $(whoami)"
 echo "Current Directory-->: $(pwd)"
 npm install
